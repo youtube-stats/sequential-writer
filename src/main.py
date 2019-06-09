b2e57a01afc6883173b1bf3ba4f852158cde6330
Salvador Guzman <guzmansalv@gmail.com>
2019-06-09 16:46:24 -0700

    simple key grabber

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+import requests
+
+api_key_server: str = 'http://localhost:8080/get'
+
+
+def get_api_key() -> str:
+    return requests.get(api_key_server).text
+
+
 def main() -> None:
-    print("Hello")
+    print(get_api_key())
 
 
 if __name__ == '__main__':
