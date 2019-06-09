ac6dd7194cdfdd1031ca3368b355aef95d338859
Salvador Guzman <guzmansalv@gmail.com>
2019-06-09 16:49:19 -0700

    simple pg conn working

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
+import atexit
+import psycopg2
 import requests
 
 api_key_server: str = 'http://localhost:8080/get'
+user: str = 'admin'
+password: str = ''
+pg_host: str = 'localhost'
+pg_port: str = '5432'
+database: str = 'youtube'
+
+
+def connect() -> psycopg2:
+    print('Connecting to db')
+    connection: psycopg2 = psycopg2.connect(
+        user=user,
+        password=password,
+        host=pg_host,
+        port=pg_port,
+        database=database)
+
+    def exit_func() -> None:
+        print('Closing connection')
+        conn.close()
+
+    atexit.register(exit_func)
+    return connection
+
+
+conn: psycopg2 = connect()
 
 
 def get_api_key() -> str:
